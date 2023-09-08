7ce1e18b0757fd4f849fbf4ac5eb7357f7602b93
gimenes <tlgimenes@gmail.com>
2023-09-08 12:24:59 -0300

    upadte item quantity

--- wake/actions/cart/updateItemQuantity.ts
+++ wake/actions/cart/updateItemQuantity.ts
@@ -4,11 +4,11 @@ import { AppContext } from "../../mod.ts";
 import { getCartCookie, setCartCookie } from "../../utils/cart.ts";
 import { fragment } from "../../utils/graphql/fragments/checkout.ts";
 import {
+  AddItemToCartMutation,
+  AddItemToCartMutationVariables,
   CheckoutFragment,
   RemoveItemFromCartMutation,
   RemoveItemFromCartMutationVariables,
-  UpdateItemQuantityMutation,
-  UpdateItemQuantityMutationVariables,
 } from "../../utils/graphql/graphql.gen.ts";
 
 export interface Props {
@@ -18,39 +18,56 @@ export interface Props {
   subscription: { subscriptionGroupId: number; recurringTypeId: number };
 }
 
+const addToCart = (
+  props: Props,
+  cartId: string,
+  ctx: AppContext,
+) =>
+  ctx.storefront.query<
+    AddItemToCartMutation,
+    AddItemToCartMutationVariables
+  >({
+    variables: {
+      input: { id: cartId, products: [props] },
+    },
+    fragments: [fragment],
+    query:
+      gql`mutation AddItemToCart($input: CheckoutProductInput!) { checkout: checkoutAddProduct(input: $input) { ...Checkout }}`,
+  });
+
+const removeFromCart = (
+  props: Props,
+  cartId: string,
+  ctx: AppContext,
+) =>
+  ctx.storefront.query<
+    RemoveItemFromCartMutation,
+    RemoveItemFromCartMutationVariables
+  >({
+    variables: {
+      input: { id: cartId, products: [{ ...props, quantity: 1e6 }] },
+    },
+    fragments: [fragment],
+    query:
+      gql`mutation RemoveItemFromCart($input: CheckoutProductInput!) { checkout: checkoutRemoveProduct(input: $input) { ...Checkout }}`,
+  });
+
 const action = async (
   props: Props,
   req: Request,
   ctx: AppContext,
 ): Promise<Partial<CheckoutFragment>> => {
-  const { storefront } = ctx;
   const cartId = getCartCookie(req.headers);
 
   if (!cartId) {
     throw new HttpError(400, "Missing cart cookie");
   }
 
-  const data = props.quantity < 1
-    ? await storefront.query<
-      RemoveItemFromCartMutation,
-      RemoveItemFromCartMutationVariables
-    >({
-      variables: {
-        input: { id: cartId, products: [{ ...props, quantity: 1e6 }] },
-      },
-      fragments: [fragment],
-      query:
-        gql`mutation RemoveItemFromCart($input: CheckoutProductInput!) { checkout: checkoutRemoveProduct(input: $input) { ...Checkout }}`,
-    })
-    : await storefront.query<
-      UpdateItemQuantityMutation,
-      UpdateItemQuantityMutationVariables
-    >({
-      variables: { input: { id: cartId, products: [props] } },
-      fragments: [fragment],
-      query:
-        gql`mutation UpdateItemQuantity($input: CheckoutProductInput!) { checkout: checkoutAddProduct(input: $input) { ...Checkout }}`,
-    });
+  let data = await removeFromCart(props, cartId, ctx);
+
+  if (props.quantity > 0) {
+    data = await addToCart(props, cartId, ctx);
+  }
 
   const checkoutId = data.checkout?.checkoutId;
   setCartCookie(ctx.response.headers, checkoutId);
